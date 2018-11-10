e3f3a1430beec651db73a031a051624cd6b0db80
Brian Field <brianfield74@yahoo.com>
2018-11-09 20:15:47 -0700

    added processenv

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/testDoris");
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, function() {
   console.log(`🌎 HEY! ==> API Server now listening on PORT ${PORT}!`)
